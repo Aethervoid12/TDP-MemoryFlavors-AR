c31a3fdf43530de37f2b401328ed75789f6bc4ca
TheAetherConfluence <92961436+Aethervoid12@users.noreply.github.com>
2022-11-18 08:56:33 +0800

    Added GroundPlane, fixed UI, added Anchoring, added Camera Toggles

---
+++ 07_#staff-feet-pics_CharSiewLaksa/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class CameraController : MonoBehaviour
+{
+    public void ToggleCamera()
+    {
+        if (VuforiaBehaviour.Instance.enabled)
+        {
+            VuforiaBehaviour.Instance.VideoBackground.StopVideoBackgroundRendering();
+            VuforiaBehaviour.Instance.enabled = false;
+        }
+        else
+        {
+            VuforiaBehaviour.Instance.VideoBackground.StartVideoBackgroundRendering();
+            VuforiaBehaviour.Instance.enabled = true;
+        }
+    }
+}
